8cd43ea9707e31dcb8e286e0c54c3e4e2ea35373
DevLobato <dev.lobatovinicius@gmail.com>
2022-04-29 13:07:34 -0300

    Fixing client.routes.js

--- backend/routes/client.routes.js
+++ backend/routes/client.routes.js
@@ -1,29 +1,66 @@
 const express = require("express")
 const {PrismaClient} = require("@prisma/client")
 
-const clients = []
-
 const clientsRoutes = express.Router()
+const prisma = new PrismaClient()
 
 // CRUD - CREATE / READ / UPDATE / DELETE
 
 // POST method for Create Client
-clientsRoutes.post("/api/createClient", (req, res) => {
-    const id = Math.floor((Date.now().toString())/100)
+clientsRoutes.post("/api/Client", async (req, res) => {
+    const { name } = req.body
+    const client = await prisma.client.create({ 
+        data: { 
+            name 
+        } 
+    })
 
-    const {name} = req.body
-    clients.push({name, id})
-
-    return res.status(201).json(clients)
+    return res.status(201).json(client)
 })
 
 // GET method for ReadAll Client
-clientsRoutes.get("/api/readAllClients", (req, res) => {
-    return res.status(200).json(clients)
+clientsRoutes.get("/api/Client", async (req, res) => {
+    const client = await prisma.client.findMany()
+
+    return res.status(200).json(client)
 })
 
 // PUT method for Update Client
-// clientsRoutes.put("/api/updateClient", (req, res) => {
+clientsRoutes.put("/api/Client", async (req, res) => {
+    const { id, name } = req.body
+
+    if(!id)
+        return res.status(400).json("Id is mandatory!")
+
+    const clientExists = await prisma.client.findUnique( { where: {id} } )
+
+    if(!clientExists)
+        return res.status(403).json("Client doesn't exists, update not authorized!")
+
+    const client = await prisma.client.update({
+        where: {
+            id, 
+        },
+        data: {
+            name
+        },
+    })
+
+    return res.status(200).json(client)
+})
+
+// DELETE method for Update Client
+// clientsRoutes.delete("/api/Client/:id", (req, res) => {
+//     const {id} = req.params
+
+//     if(!id)
+//         return res.status(400).json("Id is mandatory!")
+
+//     const clientExists = await prisma.client.findUnique( { where: {id} } )
+
+//     if(!clientExists)
+//         return res.status(403).json("Client doesn't exists, update not authorized!")
+
 
 // })
 
